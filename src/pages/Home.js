05a4cfb4107ba05a5b9992f462b2dfe2616052e5
Salia-Monica <saliamonica78@gmail.com>
2022-03-29 14:29:46 +0000

    my first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import withLayout from './withLalout'
+
+function Home () {
+  return (
+    <div>
+    <h1>welcome to my HomePage</h1>
+    <div>
+    <p>Top seed Daniil Medvedev moved a step closer to reclaiming the world number one 
+    ranking as he beat Spaniard Pedro Martinez 6-3 6-4 on Monday to reach the last  </p>
+
+    <p>Top seed Daniil Medvedev moved a step closer to reclaiming the world number one ranking as he beat 
+    Spaniard Pedro Martinez 6-3 6-4 on Monday to reach the last  </p>
+
+    <p> Top seed Daniil Medvedev moved a step closer to reclaiming the world number one ranking as he beat
+     Spaniard Pedro Martinez 6-3 6-4 on  Monday to reach the last </p>
+    </div>
+    </div>
+    
+  )
+}
+
+export default withLayout(Home)
---
+++ src/pages/withLalout.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+
+function withLalout(Component) {
+
+function Layout(){
+
+    return(
+        <div className='layout'>
+        <header className='appbar'>
+        <span>Logo</span>
+        <div>
+        <nav>
+        <span className='nav-link'><Link to="/"> Home</Link></span>
+        <span>About</span>
+        <span>Blog</span>
+        </nav>
+        </div>
+        </header>
+        <main className='main-component'>
+        <Component/>
+        </main>
+
+        <footer>&copy; 2022 PreMest</footer>
+        </div>
+    )
+}
+
+  return Layout
+}
+
+export default withLalout
